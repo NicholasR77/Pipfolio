f51870ae0abd6d397978d31ce15d73f78d4a489f
NicholasR77 <nl7robbins@icloud.com>
2022-05-22 15:52:18 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,11 @@ import Loading from './views/Loading';
 import PipfolioMain from './views/PipfolioMain';
 
 export default  function App() {
-  const [ loaded, setLoaded ] = useState(true);
+  const [ loaded, setLoaded ] = useState(false);
 
-  // setTimeout(() => {
-  //   setLoaded(true);
-  // }, 3000);
+  setTimeout(() => {
+    setLoaded(true);
+  }, 3000);
 
   return (
     <BrowserRouter>
