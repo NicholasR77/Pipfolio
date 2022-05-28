322bf8d97aa1241ddd61064a4bc9cf9acb39f98b
NicholasR77 <nl7robbins@icloud.com>
2022-05-28 07:07:31 -0700

    Update MobileMenu.tsx

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -18,7 +18,7 @@ export default function MobileMenu() {
     const menuLinks: MenuLink[] = [
         { link: '/', name: 'Home' },
         { link: '/bio', name: 'Bio' },
-        { link: '/stats', name: 'Stats' },
+        { link: '/skills', name: 'Skills' },
         { link: '/experience', name: 'Experience' },
         { link: '/education', name: 'Education' },
         { link: '/contact', name: 'Contact' }
